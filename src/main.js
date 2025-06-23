88e8746dce58daa9ae0a19bdd9de7d0b79eafacf
wclimaco2025 <william.climaco@outlook.com>
2025-06-23 10:39:38 -0600

    Plantilla Inicial del proyecto

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './style.css'
+// Header 
+const header = document.createElement('header');
+header.innerHTML = `
+    <h1>Bienvenido a PeliculasAPP</h1>
+`
+// Buscador html + javascript
+// Contenido Peliculas/Series
+// Footer
+
+
+//Inyectar HTML
+const app = document.querySelector('#app');
+app.appendChild(header);
